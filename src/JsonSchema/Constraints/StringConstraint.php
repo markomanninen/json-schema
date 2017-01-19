d1011e3ea6f44017e9c6d5c29e52ca2be8c11041
Marko Manninen <elonmedia@gmail.com>
2017-01-19 16:59:29 +0200

    support for incasesensitive patterns
    Schema:
    {
      "firstname": {
        "type": "string",
        "pattern": "alan",
        "inCaseSensitive": true
      }
    }
    
    will use -i modifier on preg_match and match with Alan, ALAN, aLaN, etc.

--- src/JsonSchema/Constraints/StringConstraint.php
+++ src/JsonSchema/Constraints/StringConstraint.php
@@ -39,7 +39,7 @@ class StringConstraint extends Constraint
         }
 
         // Verify a regex pattern
-        if (isset($schema->pattern) && !preg_match('#' . str_replace('#', '\\#', $schema->pattern) . '#u', $element)) {
+        if (isset($schema->pattern) && !preg_match('#' . str_replace('#', '\\#', $schema->pattern) . '#u'.($schema->inCaseSensitive?'i':''), $element)) {
             $this->addError($path, "Does not match the regex pattern " . $schema->pattern, 'pattern', array(
                 'pattern' => $schema->pattern,
             ));
